002ed32082606341a498e9714fa1655104d4d5f5
Fernando de Morais <mnando@gmail.com>
2016-06-21 02:19:38 -0300

    Implementação de recuperação de dados da POC.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-var BlobManager = require('./lib/blob-manager');
+const BlobManager = require('./lib/blob-manager');
 
-console.log('Iniciado com sucesso!');
+/**
+ *  Implementação de recuperação MOCK
+ **/
+const clientManager =
+  new BlobManager.ClientManager('c17992e8-44b2-41cb-b075-b9f449911e6d');
+
+clientManager.getRepositories(null, (error, result) => {
+  const data = {
+    err: error,
+    res: result,
+  };
+  console.log(data);
+});
