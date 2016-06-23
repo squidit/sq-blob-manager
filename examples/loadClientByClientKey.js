86f1ff46badd38d4f404fcbeea07c4ddc2a07473
Fernando de Morais <mnando@gmail.com>
2016-06-23 20:03:59 -0300

    Atualização das documentações de métodos do StoraManager e criação de função auxiliar par arecuperação do tamanho do Stream.

--- examples/loadClientByClientKey.js
+++ examples/loadClientByClientKey.js
@@ -1,16 +1,17 @@
 require('dotenv').config({ silent: true });
-const BlobManager = require('../lib/blob-manager');
+const { ClientManager } = require('../lib/blob-manager');
 
 /**
  *  Implementação de recuperação MOCK
  **/
 const clientManager =
-  new BlobManager.ClientManager('c17992e8-44b2-41cb-b075-b9f449911e6d');
+  new ClientManager('c17992e8-44b2-41cb-b075-b9f449911e6d');
 
-clientManager.getRepositoryInfo((error, result) => {
-  const data = {
-    err: error,
-    res: result,
-  };
-  console.log(data);
-});
+clientManager.getRepositoryInfo(
+  (error, result) => {
+    const data = {
+      err: error,
+      res: result,
+    };
+    console.log(data);
+  });
--- examples/uploadSampleBlob.js
+++ examples/uploadSampleBlob.js
@@ -1,27 +1,29 @@
 require('dotenv').config({ silent: true });
-const BlobManager = require('../lib/blob-manager');
-const MediaTypes = BlobManager.StorageManager.MediaTypes;
-var fs = require('fs');
-var path = require('path');
+const { StorageManager } = require('../lib/blob-manager');
+const MediaTypes = StorageManager.MediaTypes;
+const fs = require('fs');
+const path = require('path');
 
 /**
  *  Implementação de recuperação MOCK
  **/
 const storageManager =
-  new BlobManager.StorageManager('c17992e8-44b2-41cb-b075-b9f449911e6d');
+  new StorageManager('c17992e8-44b2-41cb-b075-b9f449911e6d');
 
 const mediaType = MediaTypes.PICTURE;
 const fileName = 'uploadSampleBlob_150x150.jpg';
 const streamPath = path.join(`${__dirname}`, `${fileName}`);
 const stream = fs.createReadStream(streamPath);
-const streamLength = fs.statSync(streamPath).size;
+const streamLength = StorageManager.getStreamLength(stream);
 
-storageManager.upload(mediaType, fileName, stream, streamLength, (error, result) => {
-  const data = {
-    err: error,
-    res: result,
-  };
-  console.log(data);
+storageManager.upload(
+  mediaType, fileName, stream, streamLength,
+  (error, result) => {
+    const data = {
+      err: error,
+      res: result,
+    };
+    console.log(data);
 
-  return true;
-});
+    return true;
+  });
